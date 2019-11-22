d15ef5bbe544e6b2dbbaea632b2754bdcddf8166
yadavanuj1996 <yadavanuj@hotmail.com>
2019-11-22 23:07:37 +0530

    Add all solutions to Info Security and QA certification.

---
+++ Information Security and Quality Assurance Certification/Information Security with HelmetJS/12-Understand BCrypt Hashes.js
@@ -0,0 +1,17 @@
+'use strict';
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const app         = express();
+const bcrypt = require('bcrypt');
+
+fccTesting(app); //For FCC testing purposes
+
+const saltRounds = 12;
+const myPlaintextPassword = 'sUperpassw0rd!';
+const someOtherPlaintextPassword = 'pass123';
+
+
+
+app.listen(process.env.PORT || 3000, () => {});
+
---
+++ Information Security and Quality Assurance Certification/Information Security with HelmetJS/13-Hash and Compare Passwords Asynchronously.js
@@ -0,0 +1,28 @@
+'use strict';
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const app         = express();
+const bcrypt = require('bcrypt');
+
+fccTesting(app); //For FCC testing purposes
+
+const saltRounds = 12;
+const myPlaintextPassword = 'sUperpassw0rd!';
+const someOtherPlaintextPassword = 'pass123';
+   
+//START_ASYNC
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash);
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res);
+  });
+});
+//END_ASYNC
+
+
+
+    
+
+app.listen(process.env.PORT || 3000, () => {});
+
